978c310f93d0c54c72192024cc27bc60e1175ca1
carlangasRC <carlos.ramirez.cuentas04@gmail.com>
2022-11-13 23:21:38 -0500

    Home Controller

---
+++ src/main/java/pe/ineapp/ineappmundialservice/Mundial/infrastructure/controller/HomeController.java
@@ -0,0 +1,17 @@
+package pe.ineapp.ineappmundialservice.Mundial.infrastructure.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("")
+public class HomeController {
+
+
+    @RequestMapping("")
+    public String Index(@RequestParam(defaultValue = "Invitado") String param){
+        return "Bienvenido".concat(param);
+    }
+}
